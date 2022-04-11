07f3e5a5246deee1613986b7bd01d193bd23fc7e
rxsss <rakhmanmagametov@gmail.com>
2022-04-11 23:33:36 +0600

    Create profile project

---
+++ ProfileOneLab/ProfileOneLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProfileOneLab
+//
+//  Created by Abdurakhman on 11.04.2022.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
